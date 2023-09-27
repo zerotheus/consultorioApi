de40eab3cd79da844134b92f503f32ba7f140660
ntheus <matheusoliveira.7313@gmail.com>
2023-09-27 02:11:49 -0300

    cadastro de paciente

--- src/main/java/apis/ifba/consultorio_api/services/PacienteServices.java
+++ src/main/java/apis/ifba/consultorio_api/services/PacienteServices.java
@@ -3,8 +3,8 @@ package apis.ifba.consultorio_api.services;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import apis.ifba.consultorio_api.Adapter.PacienteAdapter;
 import apis.ifba.consultorio_api.Dtos.Forms.PacienteForm;
+import apis.ifba.consultorio_api.adapter.PacienteAdapter;
 import apis.ifba.consultorio_api.model.Paciente;
 import apis.ifba.consultorio_api.repository.PacienteRepository;
 
