9258e65cd165873576dd87f7c9f7740c454d9773
ntheus <matheusoliveira.7313@gmail.com>
2023-09-27 20:07:12 -0300

    cadastrando usuario

--- src/main/java/apis/ifba/consultorio_api/controller/PacienteController.java
+++ src/main/java/apis/ifba/consultorio_api/controller/PacienteController.java
@@ -1,20 +1,27 @@
 package apis.ifba.consultorio_api.controller;
 
+import org.apache.catalina.connector.Response;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import apis.ifba.consultorio_api.Dtos.Forms.PacienteForm;
+import apis.ifba.consultorio_api.model.Paciente;
+import apis.ifba.consultorio_api.services.PacienteServices;
 
 @RestController
 @RequestMapping("/Paciente")
 public class PacienteController {
 
+    @Autowired
+    private PacienteServices pacienteServices;
+
     @PostMapping("/Register")
-    public String cadastraPaciente(@RequestBody PacienteForm paciente) {
-        System.out.println(paciente);
-        return paciente.toString();
+    public ResponseEntity<Paciente> cadastraPaciente(@RequestBody PacienteForm paciente) {
+        return ResponseEntity.created(null).body(pacienteServices.cadastraPaciente(paciente));
     }
 
 }
