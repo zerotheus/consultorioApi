912a1c5c3e206ed041bf1e323205de0fa95c1511
ntheus <matheusoliveira.7313@gmail.com>
2023-09-27 20:09:23 -0300

    cadastro de paciente

--- src/main/java/apis/ifba/consultorio_api/services/PacienteServices.java
+++ src/main/java/apis/ifba/consultorio_api/services/PacienteServices.java
@@ -7,16 +7,21 @@ import apis.ifba.consultorio_api.Dtos.Forms.PacienteForm;
 import apis.ifba.consultorio_api.adapter.PacienteAdapter;
 import apis.ifba.consultorio_api.model.Paciente;
 import apis.ifba.consultorio_api.repository.PacienteRepository;
+import lombok.AllArgsConstructor;
 
+@AllArgsConstructor
 @Service
 public class PacienteServices {
 
     @Autowired
     private PacienteRepository pacienteRepository;
+    private PessoaServices pessoaServices;
 
     public Paciente cadastraPaciente(PacienteForm pacienteForm) {
         PacienteAdapter pacienteAdapter = new PacienteAdapter(pacienteForm);
-        return pacienteRepository.save(pacienteAdapter.convertePacienteForm());
+        Paciente paciente = pacienteAdapter.convertePacienteForm();
+        pessoaServices.cadastraPessoa(paciente.getPessoa());
+        return pacienteRepository.save(paciente);
     }
 
 }
