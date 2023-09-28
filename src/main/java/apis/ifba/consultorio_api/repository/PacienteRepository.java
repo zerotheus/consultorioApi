787c1f4a479feb248e6a3755a6714a6953db051c
ntheus <matheusoliveira.7313@gmail.com>
2023-09-28 03:13:42 -0300

    encontra paciente pelo id da pessoa relacionada

--- src/main/java/apis/ifba/consultorio_api/repository/PacienteRepository.java
+++ src/main/java/apis/ifba/consultorio_api/repository/PacienteRepository.java
@@ -1,11 +1,16 @@
 package apis.ifba.consultorio_api.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import apis.ifba.consultorio_api.model.Paciente;
+import apis.ifba.consultorio_api.model.Pessoa;
 
 @Repository
 public interface PacienteRepository extends JpaRepository<Paciente, Long> {
+
+    Optional<Paciente> findByPessoa(Pessoa pessoa);
 
 }
