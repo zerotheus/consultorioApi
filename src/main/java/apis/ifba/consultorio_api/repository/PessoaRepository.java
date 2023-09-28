0dc96b8665df606a0d7d0b999409084e031154ad
ntheus <matheusoliveira.7313@gmail.com>
2023-09-28 03:14:10 -0300

    import removido

--- src/main/java/apis/ifba/consultorio_api/repository/PessoaRepository.java
+++ src/main/java/apis/ifba/consultorio_api/repository/PessoaRepository.java
@@ -1,5 +1,7 @@
 package apis.ifba.consultorio_api.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -9,6 +11,6 @@ import apis.ifba.consultorio_api.model.Pessoa;
 public interface PessoaRepository extends JpaRepository<Pessoa, Long> {
 
     @Query(nativeQuery = true, value = "select * from pessoas where email = ?1")
-    Pessoa findByEmail(@Param("email") String email);
+    Optional<Pessoa> findByEmail(@Param("email") String email);
 
 }
