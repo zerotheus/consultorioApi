1fa627643a891aa6368c96efee35d2f996078afa
ntheus <matheusoliveira.7313@gmail.com>
2023-10-03 17:24:26 -0300

    rota de listar pessoas, e marcar como inativas

--- src/main/java/apis/ifba/consultorio_api/controller/PacienteController.java
+++ src/main/java/apis/ifba/consultorio_api/controller/PacienteController.java
@@ -1,7 +1,13 @@
 package apis.ifba.consultorio_api.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -28,6 +34,23 @@ public class PacienteController {
     @PutMapping("/Edit/{id}")
     public ResponseEntity<Paciente> editaPaciente(@PathVariable Long id, @RequestBody PacienteForm paciente) {
         return pacienteServices.editaPaciente(id, paciente);
+    }
+
+    @GetMapping("{id}")
+    public ResponseEntity<Paciente> editaPaciente(@PathVariable Long id) {
+        return pacienteServices.encontraPacientePeloId(id);
+    }
+
+    @DeleteMapping("/Deleta/{id}")
+    public ResponseEntity<Object> deletaPaciente(@PathVariable Long id) {
+        return pacienteServices.apagaPaciente(id);
+    }
+
+    @GetMapping("/list/{page}")
+    public ResponseEntity<Page<Paciente>> listaPacientes(@PathVariable int page) {
+        // Pageable pageable = PageRequest.of(page, 10);
+        Pageable pageable = PageRequest.of(page, 10, Sort.by(Sort.Direction.ASC, "pessoa.dadosCadastrais.nome"));
+        return ResponseEntity.ok().body(pacienteServices.listaPacientes(pageable));
     }
 
 }
