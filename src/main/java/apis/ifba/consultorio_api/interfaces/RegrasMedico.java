ae18d349a715eea4b5f0fa819c4b9440603f2c46
ntheus <matheusoliveira.7313@gmail.com>
2023-10-04 16:25:54 -0300

    validacoes relacionadas a medico

--- src/main/java/apis/ifba/consultorio_api/interfaces/RegrasMedico.java
+++ src/main/java/apis/ifba/consultorio_api/interfaces/RegrasMedico.java
@@ -1,33 +1,54 @@
 package apis.ifba.consultorio_api.interfaces;
 
+import apis.ifba.consultorio_api.Dtos.Forms.MedicoForm;
 import apis.ifba.consultorio_api.enums.Especialidade;
+import apis.ifba.consultorio_api.model.Medico;
 import apis.ifba.consultorio_api.model.campos.Email;
 
 public class RegrasMedico implements RegrasEspecificasMedico {
 
+    private Medico medico;
+    private MedicoForm medicoForm;
+
+    public RegrasMedico(Medico medico, MedicoForm medicoForm) {
+        this.medico = medico;
+        this.medicoForm = medicoForm;
+    }
+
     @Override
     public boolean emailPermaneceInalterado(Email email, String novoEmail) throws Exception {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'emailPermaneceInalterado'");
+        final String emailString = email.getEmail();
+        System.out.println(emailString);
+        System.out.println(novoEmail);
+        if (!emailString.equals(novoEmail)) {
+            throw new Exception("Email nao pode ser alterado");
+        }
+        return true;
     }
 
     @Override
     public boolean validar() throws Exception {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'validar'");
+        return emailPermaneceInalterado(medico.getPessoa().getEmail(),
+                medicoForm.getPessoa().getDadosCadastrais().getEmail())
+                && crmPermaneceInalterado(medico.getCRM(), medicoForm.getCrm())
+                && especilidadePermaneceInalterada(medico.getEspecialidade(), medicoForm.getEspecialidade());
     }
 
     @Override
     public boolean crmPermaneceInalterado(String CRM, String novoCRM) throws Exception {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'crmPermaneceInalterado'");
+        if (!CRM.equals(novoCRM)) {
+            throw new Exception("Email nao pode ser alterado");
+        }
+        return true;
     }
 
     @Override
     public boolean especilidadePermaneceInalterada(Especialidade especialidade, Especialidade novaEspecialidade)
             throws Exception {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'especilidadePermaneceInalterada'");
+        if (!(especialidade == novaEspecialidade)) {
+            throw new Exception("Especialidade nao pode ser alterada");
+        }
+        return true;
     }
 
 }
