071c1fa8c3eefaaaa18567c365ffe25e89bae162
ntheus <matheusoliveira.7313@gmail.com>
2023-10-04 16:26:39 -0300

    formularios adicionados

--- src/main/java/apis/ifba/consultorio_api/Dtos/Forms/MedicoForm.java
+++ src/main/java/apis/ifba/consultorio_api/Dtos/Forms/MedicoForm.java
@@ -1,10 +1,30 @@
 package apis.ifba.consultorio_api.Dtos.Forms;
 
-import apis.ifba.consultorio_api.enums.Especialidade;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
+import apis.ifba.consultorio_api.enums.Especialidade;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@ToString
+@Getter
 public class MedicoForm {
 
-    private PessoaForm pessoa;
-    private Especialidade especialidade;
+    private final PessoaForm pessoa;
+    @Enumerated(EnumType.STRING)
+    private final Especialidade especialidade;
+    private final String crm;
+
+    @JsonCreator
+    public MedicoForm(@JsonProperty("pessoa") PessoaForm pessoa,
+            @JsonProperty("especialidade") Especialidade especialidade, @JsonProperty("crm") String crm) {
+        this.pessoa = pessoa;
+        this.especialidade = especialidade;
+        this.crm = crm;
+    }
 
 }
