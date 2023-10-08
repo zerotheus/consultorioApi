737998d262ed67ddc8e6caf466a3a0f88c08ee4a
ntheus <matheusoliveira.7313@gmail.com>
2023-10-08 19:28:00 -0300

    encontra se o paciente em consulta hoje

--- src/main/java/apis/ifba/consultorio_api/repository/ConsultaRepository.java
+++ src/main/java/apis/ifba/consultorio_api/repository/ConsultaRepository.java
@@ -1,15 +1,17 @@
 package apis.ifba.consultorio_api.repository;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import apis.ifba.consultorio_api.model.Consulta;
 
 public interface ConsultaRepository extends JpaRepository<Consulta, Long> {
 
-    @Query(nativeQuery = true, name = "a")
-    public Consulta consultaExistsToday(LocalDateTime localDateTime);
+    @Query(nativeQuery = true, value = "Select * from consultas where horario > current_date and fk_paciente_id = ?1")
+    public List<Consulta> jaPossuiConsultaAgendada(@Param(value = "id") Long id);
 
 }
