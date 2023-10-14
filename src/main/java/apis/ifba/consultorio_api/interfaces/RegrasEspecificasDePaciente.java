dbbfe58b3d67694310e5b811e5f47f892ad2debe
ntheus <matheusoliveira.7313@gmail.com>
2023-10-14 03:57:12 -0300

    Herdam de regras de pessoa agora

--- src/main/java/apis/ifba/consultorio_api/interfaces/RegrasEspecificasDePaciente.java
+++ src/main/java/apis/ifba/consultorio_api/interfaces/RegrasEspecificasDePaciente.java
@@ -2,7 +2,7 @@ package apis.ifba.consultorio_api.interfaces;
 
 import apis.ifba.consultorio_api.model.campos.CPF;
 
-public interface RegrasEspecificasDePaciente extends Regra {
+public interface RegrasEspecificasDePaciente extends RegrasDePessoas {
 
     public boolean cpfPermaneceInalterado(CPF cpf, String cpfNovo) throws Exception;
 
--- src/main/java/apis/ifba/consultorio_api/interfaces/RegrasEspecificasMedico.java
+++ src/main/java/apis/ifba/consultorio_api/interfaces/RegrasEspecificasMedico.java
@@ -2,7 +2,7 @@ package apis.ifba.consultorio_api.interfaces;
 
 import apis.ifba.consultorio_api.enums.Especialidade;
 
-public interface RegrasEspecificasMedico extends Regra {
+public interface RegrasEspecificasMedico extends RegrasDePessoas {
 
     public boolean crmPermaneceInalterado(String CRM, String novoCRM) throws Exception;
 
