0574a373c25f0db2a2dd6e8f96a3f7a34fcb0762
ntheus <matheusoliveira.7313@gmail.com>
2023-10-14 19:03:56 -0300

    encontra medicos e pacientes

--- src/main/java/apis/ifba/consultorio_api/services/ConsultaServices.java
+++ src/main/java/apis/ifba/consultorio_api/services/ConsultaServices.java
@@ -1,18 +1,21 @@
 package apis.ifba.consultorio_api.services;
 
 import java.time.LocalDateTime;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
-
 import apis.ifba.consultorio_api.Dtos.Forms.ConsultaForm;
 import apis.ifba.consultorio_api.adapter.ConsultaAdapter;
 import apis.ifba.consultorio_api.interfaces.RegrasDeMarcacaoDeConsulta;
 import apis.ifba.consultorio_api.model.Consulta;
+import apis.ifba.consultorio_api.model.Medico;
+import apis.ifba.consultorio_api.model.Paciente;
 import apis.ifba.consultorio_api.repository.ConsultaRepository;
+import apis.ifba.consultorio_api.repository.MedicoRepository;
 import apis.ifba.consultorio_api.repository.PacienteRepository;
 
 @Service
@@ -20,7 +23,10 @@ public class ConsultaServices {
 
     @Autowired
     private ConsultaRepository consultaRepository;
+    @Autowired
     private PacienteRepository pacienteRepository;
+    @Autowired
+    private MedicoRepository medicoRepository;
 
     public ResponseEntity<Consulta> marcaConsulta(ConsultaForm consultaForm) {
         Consulta consulta = converteParaConsulta(consultaForm);
@@ -30,7 +36,9 @@ public class ConsultaServices {
 
     private Consulta converteParaConsulta(ConsultaForm consultaForm) {
         ConsultaAdapter consultaAdapter = new ConsultaAdapter(consultaForm);
-        return consultaAdapter.converteConsulta(null, null);// TODO procurar medico e Paciente
+        final Paciente paciente = encontraPaciente(consultaForm);
+        encontraMedico(consultaForm);
+        return consultaAdapter.converteConsulta(paciente, null);// TODO procurar medico e Paciente
     }
 
     private void estaDentroDasRegras(ConsultaForm consultaForm) {
@@ -40,6 +48,29 @@ public class ConsultaServices {
         } catch (Exception e) {
             throw new ResponseStatusException(HttpStatusCode.valueOf(400), e.toString());
         }
+    }
+
+    private Paciente encontraPaciente(ConsultaForm consultaForm) {
+        final Optional<Paciente> paciente = pacienteEstaCadastrado(consultaForm);
+        if (paciente.isEmpty()) {
+            throw new ResponseStatusException(HttpStatusCode.valueOf(400), "O paciente deve ser informado");
+        }
+        return paciente.get();
+    }
+
+    private Medico encontraMedico(ConsultaForm consultaForm) {
+        if (consultaForm.getMedicoId() == null) {
+            System.out.println(medicoRepository.findAllAvaliable());
+        }
+        return null;
+    }
+
+    private Optional<Paciente> pacienteEstaCadastrado(ConsultaForm consultaForm) {
+        return pacienteRepository.findByIdAndStatus(consultaForm.getPacienteId(), true);
+    }
+
+    private Optional<Medico> medicoEstaCadastrado(ConsultaForm consultaForm) {
+        return medicoRepository.findByIdAndStatus(consultaForm.getMedicoId(), true);
     }
 
 }
