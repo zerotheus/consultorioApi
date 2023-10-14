53305c679623db099147126e38f1fc204253fd2d
ntheus <matheusoliveira.7313@gmail.com>
2023-10-14 03:56:26 -0300

    metodo movido para regras de pessoas

--- src/main/java/apis/ifba/consultorio_api/interfaces/Regra.java
+++ src/main/java/apis/ifba/consultorio_api/interfaces/Regra.java
@@ -3,7 +3,6 @@ package apis.ifba.consultorio_api.interfaces;
 import apis.ifba.consultorio_api.model.campos.Email;
 
 public interface Regra {
-    public boolean emailPermaneceInalterado(Email email, String novoEmail) throws Exception;
 
     public boolean validar() throws Exception;
 }
