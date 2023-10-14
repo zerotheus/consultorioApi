3e89cc826712d1f27b49aeca5e2cd5903bd2d377
ntheus <matheusoliveira.7313@gmail.com>
2023-10-14 19:02:59 -0300

    Devolve todos os ids dos medicos disponiveis

--- src/main/java/apis/ifba/consultorio_api/repository/MedicoRepository.java
+++ src/main/java/apis/ifba/consultorio_api/repository/MedicoRepository.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import apis.ifba.consultorio_api.model.Medico;
 import apis.ifba.consultorio_api.model.Pessoa;
@@ -16,5 +17,8 @@ public interface MedicoRepository extends JpaRepository<Medico, Long> {
     Optional<Medico> findByIdAndStatus(Long id, Boolean status);
 
     List<Medico> findAllByStatus(Boolean status, Pageable pageable);
+
+    @Query(nativeQuery = true, value = "Select Medico_id from medicos where status = true")
+    List<Long> findAllAvaliable();
 
 }
