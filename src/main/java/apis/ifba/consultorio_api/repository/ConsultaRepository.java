f5848f10c3b005ded6097b7a1ce94646d648dc55
ntheus <matheusoliveira.7313@gmail.com>
2023-10-16 14:11:56 -0300

    procura medicos disponiveis

--- src/main/java/apis/ifba/consultorio_api/repository/ConsultaRepository.java
+++ src/main/java/apis/ifba/consultorio_api/repository/ConsultaRepository.java
@@ -1,6 +1,7 @@
 package apis.ifba.consultorio_api.repository;
 
 import java.time.LocalDateTime;
+import java.util.Collection;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -13,5 +14,8 @@ public interface ConsultaRepository extends JpaRepository<Consulta, Long> {
 
     @Query(nativeQuery = true, value = "Select * from consultas where horario > current_date and fk_paciente_id = ?1")
     public List<Consulta> jaPossuiConsultaAgendada(@Param(value = "id") Long id);
+
+    @Query(nativeQuery = true, value = "Select fk_medico_id from consultas where not exist ?1 and horario >?2 and horario <?2 + interval '1' hour and estado != Cancelada")
+    public List<Long> medicosDisponiveis(Collection<Long> medicosIds, LocalDateTime horaMarcada);
 
 }
