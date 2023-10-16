eb5d83dc49271f6783294425c44624e544da972f
ntheus <matheusoliveira.7313@gmail.com>
2023-10-16 15:13:48 -0300

    retornando medicos indisponiveis

--- src/main/java/apis/ifba/consultorio_api/repository/ConsultaRepository.java
+++ src/main/java/apis/ifba/consultorio_api/repository/ConsultaRepository.java
@@ -1,6 +1,7 @@
 package apis.ifba.consultorio_api.repository;
 
 import java.time.LocalDateTime;
+import java.time.ZonedDateTime;
 import java.util.Collection;
 import java.util.List;
 
@@ -13,9 +14,11 @@ import apis.ifba.consultorio_api.model.Consulta;
 public interface ConsultaRepository extends JpaRepository<Consulta, Long> {
 
     @Query(nativeQuery = true, value = "Select * from consultas where horario > current_date and fk_paciente_id = ?1")
-    public List<Consulta> jaPossuiConsultaAgendada(@Param(value = "id") Long id);
+    public List<Consulta> jaPossuiConsultaAgendada(@Param(value = "id") Long id);// TODO mudar para a data e hora da
+    // consulta
 
-    @Query(nativeQuery = true, value = "Select fk_medico_id from consultas where not exist ?1 and horario >?2 and horario <?2 + interval '1' hour and estado != Cancelada")
-    public List<Long> medicosDisponiveis(Collection<Long> medicosIds, LocalDateTime horaMarcada);
-
+    // @Query(nativeQuery = true, value = "select * from (values :ids) as medicos")
+    @Query(nativeQuery = true, value = "SELECT fk_medico_id FROM consultas WHERE horario > :horaMarcada AND estado != 'Cancelada' and fk_medico_id is not null")
+    public List<Long> medicosDisponiveis(@Param("horaMarcada") LocalDateTime horaMarcada);
+    // TODO retirar not null e Renomear query
 }
