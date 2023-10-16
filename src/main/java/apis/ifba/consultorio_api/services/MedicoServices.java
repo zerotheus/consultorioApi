185f58e0705570def8c1281edc7ea5b31f5694a8
ntheus <matheusoliveira.7313@gmail.com>
2023-10-16 15:15:19 -0300

    metodo retornando todos ids

--- src/main/java/apis/ifba/consultorio_api/services/MedicoServices.java
+++ src/main/java/apis/ifba/consultorio_api/services/MedicoServices.java
@@ -1,5 +1,6 @@
 package apis.ifba.consultorio_api.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -113,6 +114,10 @@ public class MedicoServices {
 
     public Page<MedicoDto> listaMedicos(Pageable pageable) {
         return medicoRepository.findAll(pageable).map(MedicoDto::new);
+    }
+
+    public List<Long> listaTodosMedicosDisponiveis() {
+        return medicoRepository.findAllAvaliable();
     }
 
 }
