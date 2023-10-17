39e6677d106546811444cda6ed1309181fd5a6e2
ntheus <matheusoliveira.7313@gmail.com>
2023-10-17 15:17:34 -0300

    delete de cancelamento

--- src/main/java/apis/ifba/consultorio_api/controller/ConsultaController.java
+++ src/main/java/apis/ifba/consultorio_api/controller/ConsultaController.java
@@ -4,11 +4,13 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import apis.ifba.consultorio_api.Dtos.Forms.CancelamentoForm;
 import apis.ifba.consultorio_api.Dtos.Forms.ConsultaForm;
 import apis.ifba.consultorio_api.Dtos.dto.ConsultaDTO;
 import apis.ifba.consultorio_api.model.Consulta;
@@ -27,6 +29,12 @@ public class ConsultaController {
     @PostMapping("/Agendar")
     public ResponseEntity<ConsultaDTO> marcaConsulta(@RequestBody ConsultaForm consulta) {
         return ResponseEntity.created(null).body(new ConsultaDTO(consultaServices.marcaConsulta(consulta)));
+    }
+
+    @DeleteMapping("/Cancelar")
+    public ResponseEntity<Object> cancelaConsulta(@RequestBody CancelamentoForm cancelamentoForm) {
+        consultaServices.cancelar(cancelamentoForm);
+        return ResponseEntity.ok().build();
     }
 
 }
